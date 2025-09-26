ee76ae777710cf0dcd3895e61726c95067a3c846
Alok <a.thakurm007@gmail.com>
2025-09-26 08:58:14 +0530

    S01EP09 completed

--- yt-backend/src/index.js
+++ yt-backend/src/index.js
@@ -1,5 +1,13 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import express from 'express'
+const app = express();
 dotenv.config()
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT || 8000}`);
+    });
+}).catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+});
